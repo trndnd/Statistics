63fd65482fe88e88e504146a129a450f40c95c94
aaronmojica <originalcoffee20184@gmail.com>
2021-08-08 13:43:58 +0100

    Finished making function for finding the lower quarti

--- Finding_Anomalies_In_Data.py
+++ Finding_Anomalies_In_Data.py
@@ -1,19 +1,27 @@
 def FindingAnomaly(NumbersList):
     Sorted_NumbersList = NumbersList.sort()
     InterQuartileRange = Sorted_NumbersList[-1] - Sorted_NumbersList[0]
-    LowerBound = 9
-    UpperBound = float(1.5 * 9)
+    LowerQuartile,UpperQuartile = LowerAnUpperQuartile(NumbersList)
+    LowerBound = LowerQuartile - 1.5(InterQuartileRange)
+    UpperBound = UpperQuartile - 1.5(InterQuartileRange)
+    AnomaliesArray = []
+    for number in NumbersList:
+        if number < LowerBound or number > UpperBound:
+            AnomaliesArray.append(number)
 
 def LowerAnUpperQuartile(NumbersList):
-    NumberList_Length = len(NumbersList)
+    NumberListLength = len(NumbersList)
     if NumberListLength % 2 == 0:
-        LowerQuartileAndUpperQuartileForEvenLists(NumbersList,NumberList_Length)
+        LowerQuartile,UpperQuartile = LowerQuartileAndUpperQuartileForEvenLists(NumbersList)
+    else:
+        LowerQuartile,UpperQuartile = LowerQuartileAndUpperQuartileForOddLists(NumbersList)
+    return LowerQuartile,UpperQuartile
 
-def LowerQuartileAndUpperQuatrileForOddLists(NumbersList):
-    NumberListLength = len(NumbersList) - 2 #We do this not becuase of math but because of the indexing of lists
-    MedianIndexValue = (NumberListLength + 1) / 2
+def LowerQuartileAndUpperQuartileForOddLists(NumbersList):
+    NumberListLength = len(NumbersList) - 1 #We do this not becuase of math but because of the indexing of lists
+    MedianIndexValue =  (NumberListLength) / 2
     LowerQuartileList = NumbersList[:int(MedianIndexValue)]
-    UpperQuartileList = NumbersList[int(MedianIndexValue) + 1:]
+    UpperQuartileList = NumbersList[int(MedianIndexValue + 1):]
     if len(LowerQuartileList) % 2 == 0:
         LowerQuartile = FindingMedianOfAListEven(LowerQuartileList)
     else:
@@ -24,11 +32,22 @@ def LowerQuartileAndUpperQuatrileForOddLists(NumbersList):
         UpperQuartile = FindingMedianOfAListOdd(UpperQuartileList)
     return LowerQuartile,UpperQuartile
 
-def LowerQuartileAndUpperQuartileForEvenLists(NumbersList,NumberListLength):
-    Numbe
-    medianIndexNumber = round(NumberListLength/2,0)
+def LowerQuartileAndUpperQuartileForEvenLists(NumbersList):
+    NumbersListLength = len(NumbersList) # Lower it by one as lists/arrays start at 0 so a 8 long list would be still 7 numbers long but it would just at 0 and end at 7
+    MiddleIndexValue = int(round(NumbersListLength / 2,0)) # Not actaully the middle but jjust about in the middle to be able to split the array into two arrays
+    LowerQuartileList = NumbersList[:MiddleIndexValue]
+    UpperQuartileList = NumbersList[MiddleIndexValue:]
+    if len(LowerQuartileList) % 2 == 0:
+        LowerQuartile = FindingMedianOfAListEven(LowerQuartileList)
+    else:
+        LowerQuartile = FindingMedianOfAListOdd(LowerQuartileList)
+    if len(UpperQuartileList) % 2 == 0:
+        UpperQuartile = FindingMedianOfAListEven(UpperQuartileList)
+    else:
+        UpperQuartile = FindingMedianOfAListOdd(UpperQuartileList)
+    return LowerQuartile,UpperQuartile
     
-def FindingMedianOfAListEven(NumbersList):
+def FindingMedianOfAListEven(NumbersList):#Finds the mean of 2 values in the middle to find the median
     MedianLowerValueIndex = int((len(NumbersList) - 1) / 2)
     MedianUpperValueIndex = int(((len(NumbersList) - 1) / 2) + 1)
     MedianValue = (NumbersList[MedianLowerValueIndex] + NumbersList[MedianUpperValueIndex]) / 2
@@ -38,4 +57,6 @@ def FindingMedianOfAListOdd(NumbersList):
     MedianIndexValue = int((len(NumbersList) - 1)/2)
     return NumbersList[MedianIndexValue]
 
-print(LowerQuartileAndUpperQuatrileForOddLists([1,2,56,80,96,100,101]))
+print(LowerQuartileAndUpperQuartileForEvenLists([3,6,7,10,19,23,26,29,32,37]))
+print(LowerQuartileAndUpperQuartileForEvenLists([3,6,7,8,9,10]))
+print(LowerQuartileAndUpperQuartileForOddLists([10,12,16,19,23]))
